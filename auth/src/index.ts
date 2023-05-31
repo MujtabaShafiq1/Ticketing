4d4e2cf2948b64b51722ce246115d2a81b59e22e
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-05-31 16:45:27 +0500

    error handling added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+
 const app = express();
 app.use(json());
 
-app.get("api/users/currentuser" , (req, res) => {
-    res.send("Hi there")
-})
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Listening on port : ${PORT} `);
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!!!!!!");
 });
