aebd990a89ba47d6e680f760856350d2bcba56f9
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-05-31 23:06:36 +0500

    added base custom error + async errors

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -9,17 +9,15 @@ router.post(
   "/api/users/signup",
   [
     body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
-      .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
+    body("password").trim().isLength({ min: 4, max: 20 }).withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
+
     console.log("Creating a user...");
     throw new DatabaseConnectionError();
 
