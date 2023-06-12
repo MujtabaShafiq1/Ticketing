e97e9fce65a1b1b05d0691782f3a9684471e74b7
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-06-12 15:50:56 +0500

    added route functionlaities + middleware for errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,23 +1,25 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
+    name: "session",
+    maxAge: 48 * 60 * 60 * 1000,
     signed: false,
-    secure: true,
+    secure: false,
   })
 );
 
@@ -26,7 +28,7 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
@@ -34,19 +36,19 @@ app.use(errorHandler);
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error("JWT_KEY must be defined");
   }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
 
-    console.log('Connected to MongoDb');
+    console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+    console.log("Listening on port 3000!!!!!!!!");
   });
 };
 
