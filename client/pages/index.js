d4bf7ee9f971a98501e8ddd7ce2554202ff770a5
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-06-13 01:14:10 +0500

    added custom hook + signup page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,15 @@
-const HomePage = () => {
-  return <div>index</div>;
+import axios from "axios";
+
+const HomePage = ({ currentUser }) => {
+  console.log(currentUser);
+  axios.get("/api/users/currentuser");
+
+  return <h1>Landing Page</h1>;
 };
+
+// HomePage.getInitialProps = async () => {
+//   const response = axios.get("/api/users/currentuser");
+//   return response.data;
+// };
 
 export default HomePage;
