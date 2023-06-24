f1ffc6e950773fe9e63497c0274d374489c4b122
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-06-25 01:50:26 +0500

    tickets service initialized + skaffold updated

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,20 +5,10 @@ import cookieSession from "cookie-session";
 
 import { errorHandler, NotFoundError } from "@mujtaba-tickets/common";
 
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
 app.use(cookieSession({ signed: false, secure: false }));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
