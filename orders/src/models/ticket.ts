207c7a491ad2757410d79548c8f361a06d928e29
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-07-07 02:26:44 +0500

    Added test for event listeners + routes test updated

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -17,6 +17,10 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -41,8 +45,16 @@ const ticketSchema = new mongoose.Schema(
   }
 );
 
-ticketSchema.set("versionKey", "version")
-ticketSchema.plugin(updateIfCurrentPlugin)
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
+
+// important to use function here
+// ticketSchema.pre("save", function (done) {
+//   this.$where = {
+//     version: this.get("version") - 1,
+//   };
+//   done()
+// });
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
@@ -51,6 +63,14 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
     price: attrs.price,
   });
 };
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
+};
+
 ticketSchema.methods.isReserved = async function () {
   // this === the ticket document that we just called 'isReserved' on
   const existingOrder = await Order.findOne({
