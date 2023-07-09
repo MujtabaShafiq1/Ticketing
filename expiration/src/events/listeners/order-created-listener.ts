74779e59845dcc93b7201b16c4d98883dba3f9b7
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-07-09 18:27:56 +0500

    order cancelled events added

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,16 +1,24 @@
-import { Listener, OrderCreatedEvent, Subjects } from '@mujtaba-tickets/common';
-import { Message } from 'node-nats-streaming';
-import { queueGroupName } from './queue-group-name';
-import { expirationQueue } from '../../queues/expiration-queues';
+import { Listener, OrderCreatedEvent, Subjects } from "@mujtaba-tickets/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queues";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log(delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
 
     msg.ack();
   }
