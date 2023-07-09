5b582febef340da7f06fce2653590e8e4312acd6
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-07-09 19:59:12 +0500

    payment listeneres added

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -2,6 +2,10 @@ import mongoose from "mongoose";
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
 
+// Listeners
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
@@ -33,6 +37,9 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen()
+    new OrderCancelledListener(natsWrapper.client).listen()
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDb");
