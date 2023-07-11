99bd8d46b51410836286dde26abfa033e45c9f72
Mujtaba <mujtaba.shafiq213@gmail.com>
2023-07-12 03:54:28 +0500

    client updated

--- client/components/header.js
+++ client/components/header.js
@@ -1,17 +1,19 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default ({ currentUser }) => {
+const Header = ({ currentUser }) => {
   const links = [
-    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
-    !currentUser && { label: 'Sign In', href: '/auth/signin' },
-    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+    !currentUser && { label: "Sign Up", href: "/auth/signup" },
+    !currentUser && { label: "Sign In", href: "/auth/signin" },
+    currentUser && { label: "Sell Tickets", href: "/tickets/new" },
+    currentUser && { label: "My Orders", href: "/orders" },
+    currentUser && { label: "Sign Out", href: "/auth/signout" },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
       return (
         <li key={href} className="nav-item">
-          <Link className="nav-link" href={href}>
-            {label}
+          <Link href={href}>
+            <a className="nav-link">{label}</a>
           </Link>
         </li>
       );
@@ -19,8 +21,8 @@ export default ({ currentUser }) => {
 
   return (
     <nav className="navbar navbar-light bg-light">
-      <Link className="navbar-brand" href="/">
-        GitTix
+      <Link href="/">
+        <a className="navbar-brand">GitTix</a>
       </Link>
 
       <div className="d-flex justify-content-end">
@@ -29,3 +31,5 @@ export default ({ currentUser }) => {
     </nav>
   );
 };
+
+export default Header;
